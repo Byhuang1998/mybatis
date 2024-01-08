4bd9f840bb92d2761fc60b7476b80888796e84d0
byhuang1998 <byhuang1998@gmail.com>
2024-01-08 23:20:12 +0800

    use mybatis

--- src/main/java/com/byhuang/mapper/UserMapper.java
+++ src/main/java/com/byhuang/mapper/UserMapper.java
@@ -1,4 +1,8 @@
 package com.byhuang.mapper;
 
+import com.byhuang.entity.User;
+
 public interface UserMapper {
+
+    User selectUser(long id);
 }
--- src/main/java/com/byhuang/test/Test.java
+++ src/main/java/com/byhuang/test/Test.java
@@ -1,6 +1,7 @@
 package com.byhuang.test;
 
 import com.byhuang.entity.User;
+import com.byhuang.mapper.UserMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -15,8 +16,17 @@ public class Test {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // old version: 不需要定义mapper接口，直接引用xml
         try (SqlSession session = sqlSessionFactory.openSession()) {
             User user = (User) session.selectOne("com.byhuang.mapper.UserMapper.selectUser", 2);
+            System.out.println(user);
+        }
+
+        // new version: 定义mapper接口，接口名与xml中sql id一致
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            User user = mapper.selectUser(1L);
             System.out.println(user);
         }
     }
